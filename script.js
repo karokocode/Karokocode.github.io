a49a0f9b41c1ece84d1ab87199021ab874d116ad
karokocode <chess.medina.2008@gmail.com>
2024-07-30 23:13:46 -0400

    a

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    console.log('La página se ha cargado completamente.')
-    const response = await fetch('poemas.json');
-    const poemas = await response.json();
-    const container = document.querySelector('.container');
-    poemas.forEach(poema => {
-        const quoteDiv = document.createElement('div');
-        quoteDiv.className = 'quote';
-        quoteDiv.innerHTML = `
-            <p>${poema.text}</p>
-            <p class="author">- ${poema.author}</p>
-        `;
-        container.appendChild(quoteDiv);
-    });
+    console.log('La página se ha cargado completamente.');
+    try {
+        const response = await fetch('poemas.json');
+        if (!response.ok) {
+            throw new Error('Red no disponible');
+        }
+        const poemas = await response.json();
+        const container = document.querySelector('.container');
+        poemas.forEach((poema, index) => {
+            const quoteDiv = document.createElement('div');
+            quoteDiv.className = 'quote';
+            quoteDiv.innerHTML = `
+                <p>${poema.text}</p>
+                <p class="author">- ${poema.author}</p>
+            `;
+            container.appendChild(quoteDiv);
+            // Añadir una pequeña demora para la animación
+            setTimeout(() => {
+                quoteDiv.classList.add('show');
+            }, 100 * index);
+        });
+    } catch (error) {
+        console.error('Error al cargar los poemas:', error);
+    }
 });
