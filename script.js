e4916adaace578d8a3c35d3323f48fdd05a90ab4
karokocode <chess.medina.2008@gmail.com>
2024-07-30 22:40:31 -0400

    u

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('La página se ha cargado completamente.');
+document.addEventListener('DOMContentLoaded', async () => {
+    console.log('La página se ha cargado completamente.')
+    const response = await fetch('poemas.json');
+    const poemas = await response.json();
+    const container = document.querySelector('.container');
+    poemas.forEach(poema => {
+        const quoteDiv = document.createElement('div');
+        quoteDiv.className = 'quote';
+        quoteDiv.innerHTML = `
+            <p>${poema.text}</p>
+            <p class="author">- ${poema.author}</p>
+        `;
+        container.appendChild(quoteDiv);
+    });
 });
